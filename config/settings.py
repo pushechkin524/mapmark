848ef7b169e6656728ed2b1af1359e832d56b542
pushechkin <ilyamia524@gmail.com>
2025-11-03 22:17:41 +0300

    третья попытка коммит

--- config/settings.py
+++ config/settings.py
@@ -131,3 +131,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
